7ea7684f97a2971dbaf183642c61d448f8361311
fmihel <fmihel76@gmail.com>
2023-02-16 16:32:45 +0300

    use Plugin interface for session pluigin

--- source/session.php
+++ source/session.php
@@ -1,15 +1,37 @@
 <?php
 namespace fmihel;
 
+use fmihel\RouterPlugin;
+
 require_once __DIR__.'/session/iSession.php';
 require_once __DIR__.'/session/SessionDefault.php';
 
-class session {
+class session extends RouterPlugin{
+    
+    static private $session;
 
-    public static $session;
+    function __construct($sessionClass = 'fmihel\session\SessionDefault'){
+        self::$session = new  $sessionClass();
+    }
 
-    public static function use($sessionClass = 'fmihel\session\SessionDefault'){
-        session::$session = new  $sessionClass();
+    public function before($pack){
+        $to = $pack['to'];
+        
+        if ($to ==='session/autorize'){
+            $this->router::out(session::autorize($this->router::$data));        
+    
+        }if ($pack['to'] ==='session/logout'){
+    
+            session::logout();
+            $this->router::out(['session'=>[]]);        
+    
+        }else{
+            if ( !isset($pack['session']) || empty(session::autorize($pack['session']))) {
+                    $this->router::error('no autorize',['session'=>[]]);
+            }
+        }
+        return $pack;
+    
     }
 
     public static function autorize($param = []){
@@ -19,28 +41,11 @@ class session {
     public static function logout(){
         self::$session->logout();
     }
+
     public static function enabled(){
         return self::$session->enabled();
     }
 
+
+
 }
-
-
-router::on('before',function($pack){
-
-    if ($pack['to'] ==='session/autorize'){
-        
-        router::out(session::autorize(router::$data));        
-
-    }if ($pack['to'] ==='session/logout'){
-
-        session::logout();
-        router::out(['session'=>[]]);        
-
-    }else{
-        if ( !isset($pack['session']) || empty(session::autorize($pack['session']))) {
-                router::error('no autorize',['session'=>[]]);
-        }
-    }
-    return $pack;
-});
